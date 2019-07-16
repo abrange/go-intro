899d603503667fbacbf82d247d29e4d790312ff3
Álvaro Brange <alvarobrange@yahoo.es>
2019-07-15 21:39:07 -0700

    fomat document

--- functions1.go
+++ functions1.go
@@ -1,26 +1,31 @@
-# Example of function with 1 parameter and no returned value
+// Example of function with 1 parameter and no returned value
 package main
+
 import (
- "fmt"
+	"fmt"
 )
+
 // A function with one parameter with that does not return a value
 func SayHi(name string) {
- fmt.Printf("Hi %s\n", name)
+	fmt.Printf("Hi %s\n", name)
 }
+
 // A function with two parameters that does return a value (int)
 func AddTwoNumbers(a int, b int) int {
- return a + b
+	return a + b
 }
-// A function with two parameters (both int) that return also two
-// values (int)
+
+// A function with two parameters (both int) that return also two values (int)
 func Division(c, d int) (int, int) {
- quotient := c / d
- reminder := c % d
- return quotient, reminder
+	quotient := c / d
+	reminder := c % d
+	return quotient, reminder
 }
+
+// A function that does not receive any parameter
 func main() {
- SayHi("Paulina")
- fmt.Println(AddTwoNumbers(3, 9))
- q, r := Division(16, 5)
- fmt.Println("Division between", 16, " and 5 , is: Quotient ", q, " reminder ", r)
+	SayHi("Paulina")
+	fmt.Println(AddTwoNumbers(3, 9))
+	q, r := Division(16, 5)
+	fmt.Println("Division between", 16, " and 5 , is: Quotient ", q, " reminder ", r)
 }
